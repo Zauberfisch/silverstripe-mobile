d744443845e907f2a1b204d90d290b94dc968475
zauberfisch <admin@zauberfisch.at>
2013-08-18 14:27:59 +0000

    fixed canonical link (ensure that the FullSiteDomain is starts with http)

--- code/MobileSiteTreeExtension.php
+++ code/MobileSiteTreeExtension.php
@@ -5,11 +5,14 @@
 class MobileSiteTreeExtension extends DataExtension {
 	public function MetaTags(&$tags) {
 		$config = SiteConfig::current_site_config();
-
 		// Ensure a canonical link is placed, for semantic correctness and SEO
 		if (Controller::has_curr() && Controller::curr()->hasMethod("onMobileDomain") && Controller::curr()->onMobileDomain() && $config->MobileSiteType == 'RedirectToDomain') {
 			$oldBaseURL = Director::baseURL();
-			Config::inst()->update('Director', 'alternate_base_url', $config->FullSiteDomain);
+			$fullSiteDomain = $config->FullSiteDomain;
+			if(substr($fullSiteDomain,0,4) != "http") {
+				$fullSiteDomain = "http://$fullSiteDomain";
+			}
+			Config::inst()->update('Director', 'alternate_base_url', $fullSiteDomain);
 			$tags .= sprintf('<link rel="canonical" href="%s" />', $this->owner->AbsoluteLink()) . "\n";
 			Config::inst()->update('Director', 'alternate_base_url', $oldBaseURL);
 		}
